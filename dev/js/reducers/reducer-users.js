b3379a2c3f08318acf6a3f128cb7ab553d3ae0b8
ashr81 <ashrithreddi81@gmail.com>
2017-05-11 19:57:17 +0530

    Added working app code.

--- dev/js/reducers/reducer-users.js
+++ dev/js/reducers/reducer-users.js
@@ -1,33 +1,28 @@
-/*
- * The users reducer will always return an array of users no matter what
- * You need to return something, so if there are no users then just return an empty array
- * */
-
-export default function () {
-    return [
-        {
-            id: 1,
-            first: "Bucky",
-            last: "Roberts",
-            age: 71,
-            description: "Bucky is a React developer and YouTuber",
-            thumbnail: "http://i.imgur.com/7yUvePI.jpg"
-        },
-        {
-            id: 2,
-            first: "Joby",
-            last: "Wasilenko",
-            age: 27,
-            description: "Joby loves the Packers, cheese, and turtles.",
-            thumbnail: "http://i.imgur.com/52xRlm8.png"
-        },
-        {
-            id: 3,
-            first: "Madison",
-            last: "Williams",
-            age: 24,
-            description: "Madi likes her dog but it is really annoying.",
-            thumbnail: "http://i.imgur.com/4EMtxHB.png"
-        }
-    ]
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Ashrith",
+      last: "Vontari",
+      age: 25,
+      description: "Ashrith is a ruby on rails developer.",
+      thumbnail: "https://s-media-cache-ak0.pinimg.com/736x/fa/97/3c/fa973c8b02cd86e889a8fbef6b38e6c9.jpg"
+    },
+    {
+      id: 2,
+      first: "Akshay",
+      last: "Reddy",
+      age: 22,
+      description: "Is a student",
+      thumbnail: "https://s-media-cache-ak0.pinimg.com/736x/03/ff/47/03ff47ebf27b2c18952958b3fa01dcc5.jpg"
+    },
+    {
+      id: 3,
+      first: "Bucky",
+      last: "Roberts",
+      age: 71,
+      description: "Is a tutor.",
+      thumbnail: "https://lh4.ggpht.com/lCdkrR7s5qu9ifv5BUCnYZGOfFsy46tMoCM2khPKERpLowe8hN1B1LX-P5I3I0F__kU=h900"
+    }
+  ]
 }
